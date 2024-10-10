92387d0904192a2f8821f9748f216c125e2231b6
allen93075 <allen.lee@ecloudvalley.com>
2024-10-09 20:01:46 -0400

    DB_init

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,19 @@
-import express from "express";
 import cors from 'cors';
-import fs from 'fs';
+import express from "express";
 
 import dotenv from 'dotenv';
 dotenv.config();
 
-import { GoogleAIFileManager } from "@google/generative-ai/server";
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import { GoogleAIFileManager } from "@google/generative-ai/server";
+
+// DB
+import Datastore from '@seald-io/nedb';
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: '100mb'}));
-app.use(express.urlencoded({extended: true, limit: '100mb'}));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 
 //Gemini Configs
 const fileManager = new GoogleAIFileManager(process.env.API_KEY);
@@ -20,6 +22,8 @@ const model = genAI.getGenerativeModel({
   model: "gemini-1.5-flash",
 });
 
+// Init DB
+const db = new Datastore({ filename: 'data/users.db', autoload: true })
 
 /**
  * Sends an API call to gemini was 2 components: the image and prompt
@@ -29,18 +33,18 @@ const model = genAI.getGenerativeModel({
  */
 async function sendAPICall(filename, mimeType) {
   const uploadResult = await fileManager.uploadFile(filename, {
-      mimeType: mimeType,
-      displayName: "Image",
+    mimeType: mimeType,
+    displayName: "Image",
   });
 
   const result = await model.generateContent([
-      {
-        fileData: {
-          mimeType: uploadResult.file.mimeType,
-          fileUri: uploadResult.file.uri
-        }
-      },
-      { text: "What is the item in the uploaded image?"},
+    {
+      fileData: {
+        mimeType: uploadResult.file.mimeType,
+        fileUri: uploadResult.file.uri
+      }
+    },
+    { text: "What is the item in the uploaded image?" },
   ]);
   const text = result.response.text();
   return text;
@@ -52,6 +56,8 @@ async function sendAPICall(filename, mimeType) {
  * Then the image is uploaded to the local file system and the prompt is called
  */
 app.post("/upload", async (req, res) => {
+
+  // Gemini Action
   try {
     const image = req.body.image;
 
@@ -66,8 +72,32 @@ app.post("/upload", async (req, res) => {
     console.log(e);
     return res.status(400).send(e);
   }
+
+  // DB Action
+  try {
+    const image = req.body.image;
+    const base64Image = image.replace(/^data:image\/[a-z]+;base64,/, "");
+
+    const insert_image = {
+      base64Image
+    }
+
+    try {
+      const newDoc = await db.insertAsync(insert_image)
+      // newDoc is the newly inserted document, including its _id
+      // newDoc has no key called notToBeSaved since its value was undefined
+    } catch (e) {
+      // if an error happens
+      console.log(e)
+    }
+  } catch (e) {
+    console.log(e);
+    return res.status(400).send(e);
+  }
+
+  console.log("done")
 });
 
 app.listen(3000, () => {
-    console.log(`NutritionAI listening at http://localhost:3000`);
+  console.log(`NutritionAI listening at http://localhost:3000`);
 });
