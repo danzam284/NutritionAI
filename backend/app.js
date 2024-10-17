5188281310adab6f6a9d2a754fd7653b164b69e2
bbulatao <bbulatao@stevens.edu>
2024-10-16 22:27:33 -0400

    moved functions to app.js

--- backend/app.js
+++ backend/app.js
@@ -2,22 +2,138 @@ import express from "express";
 import cors from "cors";
 import fs from "fs";
 import dotenv from "dotenv";
-import { getAccessToken } from "./services/apiServices.js";
-
-import imageHandling from "./routes/imageHandling.js";
-
+import axios from "axios";
+import path from "path";
+import { fileURLToPath } from "url";
 dotenv.config();
 
-// import { GoogleAIFileManager } from "@google/generative-ai/server";
-// import { GoogleGenerativeAI } from "@google/generative-ai";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+import { GoogleAIFileManager } from "@google/generative-ai/server";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 
-// Routes (moved previous functions on app.js onto here)
-app.use("/", imageHandling);
+//Gemini Configs
+const fileManager = new GoogleAIFileManager(process.env.API_KEY);
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+/**
+ * Sends an API call to gemini was 2 components: the image and prompt
+ * @param {filename} The name of the image file to be uploaded as part of the Gemini prompt
+ * @param {mimeType} The type of the image file that is being uploaded
+ * @returns The text response of Gemini
+ */
+export async function sendAPICall(filename, mimeType) {
+  try {
+    const uploadResult = await fileManager.uploadFile(filename, {
+      mimeType: mimeType,
+      displayName: "Image",
+    });
+
+    const result = await model.generateContent([
+      {
+        fileData: {
+          mimeType: uploadResult.file.mimeType,
+          fileUri: uploadResult.file.uri,
+        },
+      },
+      { text: "what is the item in the uploaded image? Respond with just the food name." },
+    ]);
+    const rawtext = result.response.text();
+    const cleantext = rawtext.trim(); // Remove leading and trailing whitespace
+    return cleantext;
+  } catch (e) {
+    console.error("Error sending API call to Gemini:", e);
+    throw new Error("Failed to process the image.");
+  }
+}
+
+/**
+ *
+ * @param {string} text The food item that Gemini AI determined
+ * @returns object from FDC API of food nutrients
+ */
+export async function nutritionFacts(text) {
+  try {
+    const base_url = "https://api.nal.usda.gov/fdc/v1/foods/search";
+    const api_key = `?api_key=${process.env.USDA_KEY}`;
+    const query = `&query=${text}`;
+
+    // const response = await axios.get(base_url + path_url + api_key + query);
+    const response = await axios.get(base_url + api_key + query, {
+      params: {
+        pageNumber: 1,
+        pageSize: 25,
+      },
+    });
+
+    if (response) {
+      return response.data.foods[0];
+    } else {
+      throw Error(`No responses`);
+    }
+  } catch (e) {
+    console.error(e);
+    throw new Error(`FDC API failed to process`);
+  }
+}
+
+app.post("/upload", async (req, res) => {
+  try {
+    const image = req.body.image;
+
+    const mimeType = image.match(/^data:(image\/[a-zA-Z]+);base64,/)[1];
+    const base64Image = image.replace(/^data:image\/[a-z]+;base64,/, "");
+    const buffer = Buffer.from(base64Image, "base64");
+
+    const filename = `image-${Date.now()}.${mimeType.split("/")[1]}`;
+    const parentDir = path.join(__dirname, ""); // __dirname gets this directory, so get parent
+    const filepath = path.join(parentDir, filename);
+
+    // Write image into local file system
+    // fs.writeFileSync("image", buffer);
+    fs.writeFileSync(filepath, buffer);
+
+    // Prompt GenAI the image
+    // const geminiResponse = await sendAPICall("image", mimeType);
+    const geminiResponse = await sendAPICall(filename, mimeType);
+
+    // Get the macronutrients of the food from food data central api
+    let fooddata;
+    if (geminiResponse) {
+      fooddata = await nutritionFacts(geminiResponse);
+    }
+
+    let result = {
+      food: fooddata.description,
+      calories: `${fooddata.foodNutrients[3].value} ${fooddata.foodNutrients[3].unitName}`,
+      fat: `${fooddata.foodNutrients[1].value} ${fooddata.foodNutrients[1].unitName}`,
+      carbohydrates: `${fooddata.foodNutrients[2].value} ${fooddata.foodNutrients[2].unitName}`,
+      protein: `${fooddata.foodNutrients[0].value} ${fooddata.foodNutrients[0].unitName}`,
+      sodium: `${fooddata.foodNutrients[8].value} ${fooddata.foodNutrients[8].unitName}`,
+    };
+
+    // Clean up file after processing
+    if (fs.existsSync(filepath)) {
+      fs.unlinkSync(filepath); // Only delete if the file exists
+    } else {
+      console.log("Error: File not saved.");
+    }
+
+    return res.status(200).send(result);
+  } catch (e) {
+    console.log(e);
+    return res.status(400).send(e);
+  }
+});
 
 app.listen(3000, () => {
   console.log(`NutritionAI listening at http://localhost:3000`);
