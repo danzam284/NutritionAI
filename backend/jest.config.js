8f5dfa613d74751418a5e1a6b6c00cff3f5dc73d
danzam284 <45713834+danzam284@users.noreply.github.com>
2024-10-31 11:26:07 -0400

    Added several test cases

--- backend/jest.config.js
+++
@@ -1,7 +0,0 @@
-// jest.config.js
-export default {
-  testEnvironment: "node", // Set the test environment to Node.js
-  transform: {
-    "^.+\\.jsx?$": ["babel-jest", { presets: ["@babel/preset-env"] }],
-  },
-};
